71859e57222b13adbd2bb6f73f3164ced6f425db
Sergio Godinho <nikdiksnk@gmail.com>
2025-03-24 13:45:57 -0300

    Add sensor model

--- app/blueprints/sensor_api/models.py
+++ app/blueprints/sensor_api/models.py
@@ -1,5 +1,13 @@
 from app.app import db
 
 
-class Other(db.Model):
-    __tablename__ = 'other'
+class Sensor(db.Model):
+    __tablename__ = 'sensor'
+
+    sid = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    desc = db.Column(db.Text)
+    state = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'{self.sid}, {self.name}, {self.desc}, {self.state};'
